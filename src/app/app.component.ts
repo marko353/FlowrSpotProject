2809d6f64a52509098ae32196a4d5563e023dce2
Marko Stojanovic <markostojanovic353@gmail.com>
2024-02-10 12:25:04 +0100

    Refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SearchbarComponent } from './searchbar/searchbar.component';
-import { FlowergalleryComponent } from './flowergallery/flowergallery.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent,SearchbarComponent,FlowergalleryComponent],
+  imports: [RouterOutlet,NavbarComponent,SearchbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

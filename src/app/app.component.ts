7c52cb0b70b8809819a2b477fae7e4501369e1e8
Marko Stojanovic <markostojanovic353@gmail.com>
2024-02-10 11:35:15 +0100

    Searchbar component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { FlowergalleryComponent } from './flowergallery/flowergallery.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarComponent],
+  imports: [RouterOutlet,NavbarComponent,SearchbarComponent,FlowergalleryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

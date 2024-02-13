2b5f4150f4dc25394e3daa3c14d68ad32ba04e89
Marko Stojanovic <markostojanovic353@gmail.com>
2024-02-13 21:44:45 +0100

    Flower feature

--- src/app/flowergallery/flowergallery.component.ts
+++ src/app/flowergallery/flowergallery.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { FlowerGalleryItemComponent } from '../flower-gallery-item/flower-gallery-item.component';
-import { FlowerGalleryItem3Component } from '../flower-gallery-item3/flower-gallery-item3.component';
+
 
 
 @Component({
   selector: 'app-flowergallery',
   standalone: true,
-  imports: [FlowerGalleryItemComponent, FlowerGalleryItem3Component],
+  imports: [FlowerGalleryItemComponent],
   templateUrl: './flowergallery.component.html',
   styleUrl: './flowergallery.component.scss'
 })

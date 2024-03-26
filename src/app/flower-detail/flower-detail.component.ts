335da159425aaca3121c01c29e49437bee30d5d0
Marko Stojanovic <markostojanovic353@gmail.com>
2024-03-26 21:38:57 +0100

    button changes

--- src/app/flower-detail/flower-detail.component.ts
+++ src/app/flower-detail/flower-detail.component.ts
@@ -1,51 +1,45 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Flower } from '../flower';
-import { FLOWERS } from '../mock-flowers'; // Dodajte ovaj uvoz
-import { AuthService } from '../auth.service'; // Uvoz servisa za autentifikaciju
+import { FLOWERS } from '../mock-flowers';
+import { AuthService } from '../auth.service';
 import { FlowerDetailChildComponent } from '../flower-detail-child/flower-detail-child.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-flower-detail',
   templateUrl: './flower-detail.component.html',
   styleUrls: ['./flower-detail.component.scss'],
-  imports:[FlowerDetailChildComponent],
+  imports: [FlowerDetailChildComponent, CommonModule],
   standalone: true
 })
 export class FlowerDetailComponent implements OnInit {
   flower: Flower | undefined;
-  bigImageUrl: string = 'assets/images/big-placeholder.jpg'; // Fiksni URL velike slike
+  bigImageUrl: string = 'assets/images/big-placeholder.jpg';
   smallImageUrl: string | undefined;
-  isLoggedIn: boolean = false; // Dodajte promenljivu za praćenje stanja prijave
+  isLoggedIn: boolean = false;
+  isHomePage: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
-    private authService: AuthService // Dodajte servis za autentifikaciju
+    private authService: AuthService
   ) { }
 
   ngOnInit(): void {
-    // Dobavljanje ID-ja cveta iz URL parametra
     this.route.paramMap.subscribe(params => {
       const id = Number(params.get('id'));
-      // Pozivamo metod za pronalaženje biljke na osnovu ID-ja
+
       this.findFlowerById(id);
-      // Ažuriramo URL male slike kada se učita stranica
       this.updateSmallImage();
     });
-
-    // Provera da li je korisnik prijavljen kada se komponenta inicijalizuje
     this.isLoggedIn = this.authService.isLoggedIn();
   }
-
-  // Metod za pronalaženje biljke na osnovu ID-ja
   findFlowerById(id: number): void {
     this.flower = FLOWERS.find((flower) => flower.id === id);
   }
-
-  // Metod za ažuriranje URL-a male slike
   updateSmallImage(): void {
     if (this.flower) {
-      // Ažurirajte URL male slike na osnovu URL-a odgovarajućeg cveta
+
       this.smallImageUrl = this.flower.profile_picture;
     }
   }
